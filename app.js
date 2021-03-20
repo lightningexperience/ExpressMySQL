5376ad72c970a7a2621336538c061c5438d45f83
lightningexperience <lightningexperience@users.noreply.github.com>
2021-03-20 10:48:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -69,4 +69,6 @@ app.get('/search/:name', (request, response) => {
     .catch(err => console.log(err));
 })
 
-app.listen(process.env.PORT, () => console.log('app is running'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
